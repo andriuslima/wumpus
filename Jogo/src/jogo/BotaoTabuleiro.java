06375c6c9a25e1181965139fc12b1517442bbf47
Ravazolo88 <prdrosa@inf.ufpel.edu.br>
2023-09-16 12:37:34 -0300

    16/09 Mudanças criação botão tabuleiro e mudança no tabuleiro

---
+++ Jogo/src/jogo/BotaoTabuleiro.java
@@ -0,0 +1,22 @@
+package jogo;
+
+import javax.swing.JButton;
+
+
+public class BotaoTabuleiro extends JButton {
+    int posX;
+    int posY;
+    
+    public BotaoTabuleiro(int posX, int posY){
+        this.posX = posX;
+        this.posY = posY;
+    } 
+    
+    public int getPosX(){
+        return posX;
+    }
+    
+    public int getPosY(){
+        return posY;
+    }
+}
